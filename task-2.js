7e9f1369b11f0300f36ddfb06bdc25e3e94df47d
skmajumder <skmajumder.cse@gmail.com>
2023-07-25 20:50:30 +0600

    task 2

---
+++ task-2.js
@@ -0,0 +1,19 @@
+/**
+ * * Task 2 : Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array.
+ */
+
+function add(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      sum += arr[i];
+    }
+  }
+  return sum;
+}
+
+const numbers = [2, -5, 10, -3, 7];
+const result = add(numbers);
+console.log(
+  `The sum of all positive numbers in the array [${numbers}] is: ${result}`
+);

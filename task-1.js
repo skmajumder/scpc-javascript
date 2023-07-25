c208970536fac12fdc0defe2bcbabd829f9d3469
skmajumder <skmajumder.cse@gmail.com>
2023-07-25 20:19:55 +0600

    Task 1

---
+++ task-1.js
@@ -0,0 +1,19 @@
+/**
+ ** Task 1 : Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method.
+ */
+
+function reverseString(input) {
+  const clearInput = input.replace(/[^a-zA-Z0-9\s]/g, "");
+  let reverseStr = "";
+
+  for (let i = clearInput.length - 1; i >= 0; i--) {
+    reverseStr += clearInput.charAt(i);
+  }
+  return reverseStr;
+}
+
+const inputString = "hello world";
+const revereString = reverseString(inputString);
+
+console.log(`Original string: ${inputString}`);
+console.log(`Reverse string: ${revereString}`);
